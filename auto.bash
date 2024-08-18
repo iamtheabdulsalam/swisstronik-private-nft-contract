c3208126394524291539d4fb36daee5716738770
Abdulsalam Abdulsalam <0xdudul@gmail.com>
2024-08-18 14:52:59 +0000

    feat: Completed private NFT

---
+++ auto.bash
@@ -0,0 +1,16 @@
+npm i 
+npm run compile
+
+npm run deploy
+npm run deploy
+npm run deploy
+npm run deploy
+
+npm run mint
+npm run mint
+npm run mint
+
+rm -rf .git
+git init
+git add .
+git commit -m "feat: Completed private NFT"
---
+++ utils/deployed-address.ts
@@ -0,0 +1,3 @@
+const deployedAddress = '0xa52DDecBbeE07cA67f4839976eB9ecc8464Ecd40'
+
+export default deployedAddress
